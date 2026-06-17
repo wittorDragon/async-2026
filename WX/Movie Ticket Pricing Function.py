47fc559dfc8013557b389376300227647a3ebb8b
Sirawit <6710301021@cdti.ac.th>
2026-06-17 11:54:38 +0700

    New_Movie Ticket Pricing Function.py

---
+++ WX/Movie Ticket Pricing Function.py
@@ -0,0 +1,25 @@
+import sys
+
+def calculate_ticket_price(age):
+    # --- เขียนโค้ดของนักเรียนในส่วนนี้ / Write your code here ---
+    if age < 12:
+        return 120
+    elif age <= 60:
+        return 200
+    else:
+        return 150
+    # --------------------------------------------------------
+
+def main():
+    # เปลี่ยนมาเช็ก > 1 และใช้ sys.argv[-1] เพื่อความแม่นยำใน VPL
+    if len(sys.argv) > 1:
+        test_age = int(sys.argv[-1])
+        result = calculate_ticket_price(test_age)
+        print(result)
+    else:
+        test_age = 25
+        result = calculate_ticket_price(test_age)
+        print(f"Age: {test_age} -> Ticket Price: {result} Baht")
+
+if __name__ == "__main__":
+    main()
