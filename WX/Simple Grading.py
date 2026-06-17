7fb00340389db936613c21336eca7c77ab40e7fa
Sirawit <6710301021@cdti.ac.th>
2026-06-17 19:52:31 +0700

    Final

--- WX/Simple Grading.py
+++ WX/Simple Grading.py
@@ -2,8 +2,10 @@ import sys
 def evaluate_grade(score):
     if score >= 80:          # คะแนนตั้งแต่ 80 ขึ้นไป (score >= 80) ส่งค่ากลับเป็น "Excellent"
         return "Excellent"
+    
     elif score >= 50:            # คะแนนตั้งแต่ 50 ถึง 79 (50 <= score < 80) ส่งค่ากลับเป็น "Pass"
         return "Pass"
+    
     else :                 # คะแนนต่ำกว่า 50 (score < 50) ส่งค่ากลับเป็น "Fail"
         return "Fail"
 
