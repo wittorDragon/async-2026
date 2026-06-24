694a3443f28b5e7a51465f5cb263cde35cd9b9f6
Sirawit <6710301021@cdti.ac.th>
2026-06-24 11:10:04 +0700

    Week1/pid04_Finish

--- Week1/pid04_asyncio.py
+++ Week1/pid04_asyncio.py
@@ -3,12 +3,56 @@ import asyncio
 import os
 import threading
 
-# ฟังก์ชันจำลองการทำกาแฟแบบ Asynchronous
+# ฟังก์ชันจำลองการทำงานแบบ Asynchronous
 async def make_coffee(customer_name):
-    pass
+    # 1. ดู Process ID และ Thread ID (จะเหมือนกันหมดทั้งทุกค่า)
+    pid = os.getpid()
+    thread_id = threading.current_thread().native_id
+
+    # 2. ดูข้อมูล Task ปัจจุบันของ asyncio
+    current_task = asyncio.current_task()
+    task_name = current_task.get_name()  # ชื่อ Task
+
+    # ใน Python 3.12+ หากอยากได้เลข Unique ID ของ Task ให้ใช้ id(current_task)
+    task_id = id(current_task)
+
+    print(f"{ctime()} | [PID: {pid}] [TID: {thread_id}] [Async Task ID: {task_id}] [Task Name: {task_name}] กำลังชงกาแฟให้ ลูกค้า {customer_name}...")
+    
+    # จุดที่อนุญาต Non-blocking wait
+    await asyncio.sleep(5)
+
+    print(f"{ctime()} | [PID: {pid}] [TID: {thread_id}] [Async Task ID: {task_id}] [Task Name: {task_name}] ลูกค้า {customer_name}: ได้รับกาแฟแล้ว!")
 
 async def main():
-    pass
+    queue = ['A', 'B', 'C']
+
+    main_pid = os.getpid()
+    main_tid = threading.current_thread().native_id
+
+    print(f"{ctime()} | [Main PID: {main_pid}] [Main TID: {main_tid}] === เริ่มระบบจำลองชงกาแฟแบบ asyncio ===")
+
+    start_time = time()
+
+    tasks = []
+
+    for customer in queue:
+        # สร้าง Coroutine
+        coro = make_coffee(customer)
+
+        # แปลง Coroutine ให้เป็น Task เพื่อให้ Event Loop บริหาร และสลับคิวได้
+        task = asyncio.create_task(
+            coro,
+            name=f"Task-{customer}"
+        )
+
+        tasks.append(task)
+
+    # รอให้ทำงานจบ
+    await asyncio.gather(*tasks)
+
+    duration = time() - start_time
+
+    print(f"{ctime()} | ใช้เวลารวมทั้งหมด: {duration:0.2f} วินาที")
 
 if __name__ == "__main__":
     asyncio.run(main())
