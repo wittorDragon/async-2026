6eff5e563dadb7682860ad0189dc6d4ac6c08653
Sirawit <6710301021@cdti.ac.th>
2026-06-24 11:02:54 +0700

    Week1/pid03_Finish

--- Week1/pid03_multiprocess.py
+++ Week1/pid03_multiprocess.py
@@ -5,10 +5,42 @@ import os
 
 # ฟังก์ชันจำลองการทำกาแฟให้ลูกค้า 1 คน
 def make_coffee(customer_name):
-    pass
+    # ดึง PID ของแต่ละโปรเซส (ซึ่งจะแยกกันเด็ดขาด)
+    pid = os.getpid()
+    thread_id = threading.current_thread().native_id
+    thread_name = threading.current_thread().name
+
+    print(f"{ctime()} | [PID: {pid}] [TID: {thread_id}] [Thread Name: {thread_name}] กำลังชงกาแฟให้ ลูกค้า {customer_name}...")
+    sleep(5)  # แยกการทำงานของ Thread นี้ไว้ 5 วินาทีเต็ม
+    print(f"{ctime()} | [PID: {pid}] [TID: {thread_id}] [Thread Name: {thread_name}] ลูกค้า {customer_name} ได้รับกาแฟแล้ว!")
 
 def main():
-    pass
+    queue = ['A', 'B', 'C']
+
+    main_pid = os.getpid()
+    main_tid = threading.current_thread().native_id
+
+    print(f"{ctime()} | [Main PID: {main_pid}] [Main TID: {main_tid}] === เริ่มระบบจำลองชงกาแฟแบบ Multi-processing ===")
+    start_time = time()
+
+    processes = []
+
+    # ลูปการทำงาน process
+    for customer in queue:
+        # สร้าง Process ใหม่แยกจากกันเด็ดขาด
+        p = multiprocessing.Process(
+            target=make_coffee,
+            args=(customer,)
+        )
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    duration = time() - start_time
+
+    print(f"{ctime()} | ใช้เวลารวมทั้งหมด: {duration:0.2f} วินาที")
 
 if __name__ == "__main__":
     main()
